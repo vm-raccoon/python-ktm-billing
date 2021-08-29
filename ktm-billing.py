930d87482837e12e807bcf704771fabb3f1671b5
vm-raccoon <vm.raccoon@gmail.com>
2021-08-29 18:51:26 +0300

    Add class TelegramBot: send message to chat

--- ktm-billing.py
+++ ktm-billing.py
@@ -1,6 +1,7 @@
 from classes.Config import Config
 from classes.KTM import KTM
 from classes.Database import Database as DB
+from classes.TelegramBot import TelegramBot
 from datetime import date
 
 
@@ -12,9 +13,15 @@ for item in Config(__file__, "config.json").read():
     db = DB(item["sqlite"])
     lastRow = db.getLastHistoryRow()
 
-    if compareDateToday(lastRow["datetime"]):
+    if lastRow and compareDateToday(lastRow["datetime"]):
         continue
 
-    ktm = KTM(item["ktm-account"])
-    overview = ktm.getOverview()
+    overview = KTM(item["ktm-account"]).getOverview()
     db.insert(overview)
+
+    overviewCopy = overview.copy()
+    overviewCopy["username"] = item["ktm-account"]["username"]
+    overviewCopy["diff"] = round(lastRow["balance"] - overview["balance"], 2) if lastRow else 0
+
+    bot = TelegramBot(item["telegram"]["bot-token"])
+    bot.sendMessage(item["telegram"]["chat_id"], overviewCopy)
